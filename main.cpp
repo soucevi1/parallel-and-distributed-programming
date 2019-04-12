fe6725cd0c5a588f8d4aa1a983e31e5225a8a194
Vít Souček <soucevi1@fit.cvut.cz>
2019-04-12 18:42:53 +0200

    Refactored dynamic scheduling

--- main.cpp
+++ main.cpp
@@ -28,8 +28,6 @@ struct tile_info {
     int cn;
 };
 
-tile_info get_tile_info(ifstream ifstream);
-
 string get_filename(int argc, char **argv) {
     int opt;
     string filename;
@@ -116,22 +114,6 @@ vector<coords> get_forbidden_fields(ifstream &f) {
     return v;
 }
 
-vector<int> precalculate_problem_counts(int problems, int proc_count) {
-    vector<int> prob_counts(proc_count);
-
-    prob_counts[0] = 0;
-
-    int assigned = 0;
-    for (int i = 1; i < proc_count - 1; i++) {
-        prob_counts[i] = (int) (problems / (proc_count - 1));
-        assigned += prob_counts[i];
-    }
-
-    prob_counts[proc_count - 1] = problems - assigned;
-
-    return prob_counts;
-}
-
 solver generate_solver(string &filename) {
 
     dimensions dims = dimensions();
@@ -161,53 +143,6 @@ solver generate_solver(string &filename) {
     return s;
 }
 
-void send_problem_counts(int proc_count, vector<int> &count_problems_to_send) {
-    for (int i = 1; i < proc_count; i++) {
-        MPI_Send(&count_problems_to_send[i], 1, MPI_INT, i, COUNT_TAG, MPI_COMM_WORLD);
-    }
-}
-
-void send_subproblems(int proc_count, vector<int> &count_problems_to_send, solver &s) {
-    for (int i = 0; i < proc_count; i++) {
-        for (int j = 0; j < count_problems_to_send[i]; j++) {
-
-            solution sol = s.initial_solutions.front().starting_solution;
-            coords pos = s.initial_solutions.front().position;
-            s.initial_solutions.pop_front();
-
-            comm_info c = comm_info(sol, pos, s.best_solution);
-
-            string cs = c.serialize();
-
-            MPI_Send(cs.c_str(), cs.length(), MPI_CHAR, i, WORK_TAG, MPI_COMM_WORLD);
-        }
-    }
-}
-
-solution get_slave_solution() {
-    MPI_Status stat;
-    MPI_Probe(MPI_ANY_SOURCE, FINAL_TAG, MPI_COMM_WORLD, &stat);
-
-    int recv_len;
-    MPI_Get_count(&stat, MPI_CHAR, &recv_len);
-    vector<char> m(recv_len);
-
-    MPI_Recv(&m[0], recv_len, MPI_CHAR, MPI_ANY_SOURCE, FINAL_TAG, MPI_COMM_WORLD, &stat);
-    solution recvd_sol = solution(string(m.begin(), m.end()));
-
-    return recvd_sol;
-}
-
-solution choose_the_best_solution(vector<solution> &slave_solutions) {
-    solution the_best = slave_solutions[0];
-    for (int i = 1; i < slave_solutions.size(); i++) {
-        if (slave_solutions[i].cost > the_best.cost) {
-            the_best = slave_solutions[i];
-        }
-    }
-    return the_best;
-}
-
 int get_idle_slave(vector<bool> slave_work) {
     int worker = 0;
     for (int i = 1; i < slave_work.size(); i++) {
@@ -219,21 +154,66 @@ int get_idle_slave(vector<bool> slave_work) {
     return worker;
 }
 
+
+
+
+solution receive_solution(MPI_Status &stat){
+    int recvd;
+    MPI_Get_count(&stat, MPI_CHAR, &recvd);
+    vector<char> m(recvd);
+    MPI_Recv(&m[0], recvd, MPI_CHAR, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
+    solution recvd_solution = solution(string(m.begin(), m.end()));
+    return recvd_solution;
+}
+
+void send_work_to_slave(int idle_slave, solver & s, solution & master_best){
+    solution sol = s.initial_solutions.front().starting_solution;
+    coords pos = s.initial_solutions.front().position;
+    s.initial_solutions.pop_front();
+
+    comm_info c = comm_info(sol, pos, master_best);
+
+    string cs = c.serialize();
+
+    MPI_Send(cs.c_str(), cs.length(), MPI_CHAR, idle_slave, WORK_TAG, MPI_COMM_WORLD);
+}
+
+vector<bool> stop_idle_slaves(int proc_count, vector<bool> & slave_working){
+    vector<bool>slaves_stopped(proc_count);
+    for (int i = 1; i < slave_working.size(); i++) {
+        if (!slave_working[i]) {
+            int k=1;
+            MPI_Send(&k, 1, MPI_INT, i, STOP_TAG, MPI_COMM_WORLD);
+            slaves_stopped[i] = true;
+        } else{
+            slaves_stopped[i] = false;
+        }
+    }
+    return slaves_stopped;
+}
+
+comm_info receive_comm_info(MPI_Status & stat){
+    int recvd;
+    MPI_Get_count(&stat, MPI_CHAR, &recvd);
+    vector<char> m(recvd);
+    MPI_Recv(&m[0], recvd, MPI_CHAR, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
+
+    comm_info recvd_c = comm_info(string(m.begin(), m.end()));
+
+    return recvd_c;
+}
+
 int main(int argc, char **argv) {
 
     int my_rank;
-
-
     MPI_Init(&argc, &argv);
-
     MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
 
     int proc_count;
     MPI_Comm_size(MPI_COMM_WORLD, &proc_count);
 
-    // ------- MASTER -----------------------------------------------
+    // ================================ MASTER ======================================
     if (my_rank == 0) {
-
         // Read input file
         string filename = get_filename(argc, argv);
 
@@ -245,39 +225,36 @@ int main(int argc, char **argv) {
             return 1;
         }
 
+        // TIME MEASURE BEGINS
         clock_t begin_measure = clock();
 
         int required_levels = 4;
+
+        // Generate subsolutions for the slaves
         s.generate_solutions_for_slaves(required_levels);
         int q_size = (int) s.initial_solutions.size();
         cout << "Master: subproblems generated: " << q_size << endl;
 
-        int working_slaves = 0;
-
-        vector<bool> slave_working(proc_count);
+        vector<bool> slave_working(proc_count, false);
         slave_working[0] = true;
-        for (int i = 1; i < proc_count; i++) {
-            slave_working[i] = false;
-        }
 
         solution master_best = s.best_solution;
 
         int received_solutions = 0;
-
         int sent_work = 0;
 
+        // Send all the subproblems to the slaves
         while (!s.initial_solutions.empty()) {
 
             MPI_Status stat;
             int flag;
+
+            // Is message available?
             MPI_Iprobe(MPI_ANY_SOURCE, FINAL_TAG, MPI_COMM_WORLD, &flag, &stat);
 
+            // Message available -- read the message
             if (flag) {
-                int recvd;
-                MPI_Get_count(&stat, MPI_CHAR, &recvd);
-                vector<char> m(recvd);
-                MPI_Recv(&m[0], recvd, MPI_CHAR, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
-                solution recvd_solution = solution(string(m.begin(), m.end()));
+                solution recvd_solution = receive_solution(stat);
 
                 if (recvd_solution.cost > master_best.cost) {
                     master_best = recvd_solution;
@@ -285,63 +262,32 @@ int main(int argc, char **argv) {
 
                 slave_working[stat.MPI_SOURCE] = false;
                 received_solutions++;
-                working_slaves--;
                 continue;
             }
 
+            // Is there a worker that has nothing to do?
             int idle_slave = get_idle_slave(slave_working);
             if (idle_slave == 0) {
                 continue;
             }
 
-            solution sol = s.initial_solutions.front().starting_solution;
-            coords pos = s.initial_solutions.front().position;
-            s.initial_solutions.pop_front();
-
-            comm_info c = comm_info(sol, pos, master_best);
-
-            string cs = c.serialize();
-
-            MPI_Send(cs.c_str(), cs.length(), MPI_CHAR, idle_slave, WORK_TAG, MPI_COMM_WORLD);
+            // Send subproblem to an idle slave
+            send_work_to_slave(idle_slave, s, master_best);
             slave_working[idle_slave] = true;
             sent_work++;
-            working_slaves++;
         }
 
-        vector<bool>slaves_stopped(proc_count);
-        for (int i = 1; i < slave_working.size(); i++) {
-            if (!slave_working[i]) {
-                int k=1;
-                MPI_Send(&k, 1, MPI_INT, i, STOP_TAG, MPI_COMM_WORLD);
-                slaves_stopped[i] = true;
-            } else{
-                slaves_stopped[i] = false;
-            }
-        }
+        // All subproblems sent, tell idle slaves to exit
+        vector<bool> slaves_stopped = stop_idle_slaves(proc_count, slave_working);
 
-        while (received_solutions < sent_work/*working_slaves > 0*/) {
-         /*   cout << "Waiting for " << sent_work - received_solutions << " responses" << endl;
-            cout << "---working:" << endl;
-            for (int i = 1; i < slave_working.size(); i++) {
-                if (slave_working[i]) {
-                    cout << i << endl;
-                }
-            }
-            cout << "---" << endl;*/
-
+        // Wait for the last solutions
+        while (received_solutions < sent_work) {
             MPI_Status stat;
             MPI_Probe(MPI_ANY_SOURCE, FINAL_TAG, MPI_COMM_WORLD, &stat);
 
-
-            int recv_len;
-            MPI_Get_count(&stat, MPI_CHAR, &recv_len);
-            vector<char> m(recv_len);
-
-            MPI_Recv(&m[0], recv_len, MPI_CHAR, MPI_ANY_SOURCE, FINAL_TAG, MPI_COMM_WORLD, &stat);
-            solution recvd_sol = solution(string(m.begin(), m.end()));
+            solution recvd_sol = receive_solution(stat);
 
             received_solutions++;
-            working_slaves--;
             slave_working[stat.MPI_SOURCE] = false;
 
             if (recvd_sol.cost > master_best.cost) {
@@ -349,29 +295,29 @@ int main(int argc, char **argv) {
             }
         }
 
-        cout << "Stopping slaves" << endl;
-        for (int i = 1; i < proc_count; i++) {
-            if(!slaves_stopped[i]) {
-                int k = 0;
-                MPI_Send(&k, 1, MPI_INT, i, STOP_TAG, MPI_COMM_WORLD);
-            }
-        }
+        // Stop all the remaining slaves now
+        slaves_stopped = stop_idle_slaves(proc_count, slave_working);
 
         cout << "The BEST solution is:" << endl;
         master_best.print_solution();
 
+        // TIME MEASURE ENDS
         clock_t end_measure = clock();
         double elapsed_secs = double(end_measure - begin_measure) / CLOCKS_PER_SEC;
 
         cout << "Finished in " << elapsed_secs << " seconds." << endl;
 
 
-        // ------- SLAVE -----------------------------------------------
+        // ================================ SLAVE ======================================
     } else {
 
         int work_id = 0;
+
+        // Work until told otherwise
         while (true) {
             MPI_Status stat;
+
+            // Wait for a message
             MPI_Probe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &stat);
 
             bool finish = false;
@@ -379,6 +325,7 @@ int main(int argc, char **argv) {
             switch (tag) {
 
                 case STOP_TAG: {
+                    // Master wants the slave to stop
                     int k;
                     MPI_Recv(&k, 1, MPI_INT, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
                     finish = true;
@@ -386,15 +333,10 @@ int main(int argc, char **argv) {
                     break;
 
                 case WORK_TAG: {
-                    int recvd;
-                    MPI_Get_count(&stat, MPI_CHAR, &recvd);
-                    vector<char> m(recvd);
-                    MPI_Recv(&m[0], recvd, MPI_CHAR, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
-                    comm_info recvd_c = comm_info(string(m.begin(), m.end()));
-
+                    // Master sends new work
+                    comm_info recvd_c = receive_comm_info(stat);
                     solver s = solver(recvd_c, proc_count, my_rank);
                     s.solve();
-
                     string best_serial = s.best_solution.serialize();
                     MPI_Send(best_serial.c_str(), best_serial.length(), MPI_CHAR, 0, FINAL_TAG, MPI_COMM_WORLD);
                     work_id++;
@@ -402,15 +344,14 @@ int main(int argc, char **argv) {
                     break;
 
                 case NEW_BEST_TAG: {
-                    int recvd;
-                    MPI_Get_count(&stat, MPI_CHAR, &recvd);
-                    vector<char> m(recvd);
-                    MPI_Recv(&m[0], recvd, MPI_CHAR, stat.MPI_SOURCE, stat.MPI_TAG, MPI_COMM_WORLD, &stat);
+                    // Idle slave received a message with someone else's best solution
+                    // Receive the message and throw it away
+                    solution sol = receive_solution(stat);
                 }
                     break;
 
                 default: {
-
+                    // Slave received a message it does not understand
                     cout << my_rank << ": Got unexpected message with tag " << stat.MPI_TAG << " from "
                          << stat.MPI_SOURCE << " error: " << stat.MPI_ERROR << endl;
                 }
